167cefc91c07ceaefd6dfc779a26a481dbe79e57
Ayush More <ayush08am@gmail.com>
2025-11-28 16:38:39 +0530

    Update FirebaseConfig.js

--- src/Firebase/FirebaseConfig.js
+++ src/Firebase/FirebaseConfig.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore"
 import {getStorage} from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtiVFjnUXTxsfcIbuzz7EwxYkAJ-z0-2I",
-  authDomain: "fooddev1-b4c28.firebaseapp.com",
-  projectId: "fooddev1-b4c28",
-  storageBucket: "fooddev1-b4c28.firebasestorage.app",
-  messagingSenderId: "931767479331",
-  appId: "1:931767479331:web:5791c46c7ef8a5f9569596"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",//your firebase code
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
